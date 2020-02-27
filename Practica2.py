aa786a3a252460f826a5d028d8bddfbde1ac19fd
Daniel Ortiz Manjarrez <pelondani22000@gmail.com>
2020-02-27 11:52:18 -0600

    commit algo

---
+++ Practica2.py
@@ -0,0 +1,26 @@
+import os
+
+red ="200.33.171.0/24"
+
+os.system("nmap -sP " + red)
+
+# Starting Nmap 7.60 ( https://nmap.org ) at 2020-02-27 11:47 CST
+# Nmap scan report for mail.itmorelia.edu.mx (200.33.171.3)
+# Host is up (0.0035s latency).
+# Nmap scan report for 200.33.171.13
+# Host is up (0.0018s latency).
+# Nmap scan report for 200.33.171.63
+# Host is up (0.017s latency).
+# Nmap scan report for sagitario.itmorelia.edu.mx (200.33.171.65)
+# Host is up (0.0029s latency).
+# Nmap scan report for 200.33.171.99
+# Host is up (0.0057s latency).
+# Nmap scan report for 200.33.171.115
+# Host is up (0.0026s latency).
+# Nmap scan report for vinculacion.itmorelia.edu.mx (200.33.171.124)
+# Host is up (0.0045s latency).
+# Nmap scan report for 200.33.171.125
+# Host is up (0.0045s latency).
+# Nmap scan report for 200.33.171.127
+# Host is up (0.0089s latency).
+# Nmap done: 256 IP addresses (9 hosts up) scanned in 3.47 seconds
